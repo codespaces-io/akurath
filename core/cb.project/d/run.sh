be171c08fd21c4800335c19d5fcbe49904874086
Gourav Shah <gs@initcron.org>
2017-02-17 14:39:06 +0000

    disable entrypoint code for now, just connect to the node

--- core/cb.project/d/run.sh
+++ core/cb.project/d/run.sh
@@ -14,20 +14,25 @@ WORKSPACE=$1
 PORT=$2
 NODENUM=$3
 
-# Get plausible entry point
-entry_point=$(${FGREP} "[int|void] main\(" -R ${WORKSPACE} | tr ':' '\n' | grep ".d" | head -n 1)
+#AKUNOTE: forget about entryping for now. Just go ahead and connect to the nodes
+## Get plausible entry point
+#entry_point=$(${FGREP} "[int|void] main\(" -R ${WORKSPACE} | tr ':' '\n' | grep ".d" | head -n 1)
 
-# No entry point found
-# default to main.d
-if [ -z "$entry_point" ]; then
-    entry_point="./main.d"
-fi
+## No entry point found
+## default to main.d
+#if [ -z "$entry_point" ]; then
+#    entry_point="./main.d"
+#fi
+#
+#if [ -f ${entry_point} ]; then
+##    exec dmd -run ${entry_point}
+#   echo "connecting to node$NODENUM..."
+#   ssh devops@node$NODENUM
+#else
+#    # Exit
+#    exit 1
+#fi
 
-if [ -f ${entry_point} ]; then
-#    exec dmd -run ${entry_point}
-   echo "connecting to node$NODENUM..."
-   ssh devops@node$NODENUM
-else
-    # Exit
-    exit 1
-fi
+echo "connecting to node$NODENUM..."
+ssh devops@node$NODENUM
+
